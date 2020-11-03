5b332184e45d3eed41f5b426a87a65413b488dfa
Sidany Walker <sidanyk@gmail.com>
2020-11-03 09:19:25 -0500

    Initial Commit

---
+++ SidTalesV2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SidTalesV2
+//
+//  Created by Sidany Walker on 11/3/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
